df8c2c1d7b1168e066f30d39a451f855a51fb534
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-07-11 20:43:46 +0900

    add roll dice

--- contracts/Kuji.sol
+++ contracts/Kuji.sol
@@ -13,9 +13,16 @@ contract Kuji is VRFConsumerBase, Ownable {
     uint256 internal fee;
 
     uint256 public randomResult;
-    uint16 public constant WINNING_NUMBER = 7;
+    uint256 public constant WINNING_NUMBER = 7;
+    uint256 public constant ROLL_IN_PROGRESS = 42;
+
+    mapping(bytes32 => address) public s_rollers;
+    mapping(address => uint256) public s_results;
 
     event NumberChanged(bytes32 requestId, uint256 randomness);
+
+    event DiceRolled(bytes32 indexed requestId, address indexed roller);
+    event DiceLanded(bytes32 indexed requestId, uint256 indexed result);
 
     /**
      * Constructor inherits VRFConsumerBase
@@ -40,14 +47,27 @@ contract Kuji is VRFConsumerBase, Ownable {
     }
 
     /**
-     * Requests randomness
+     * @notice Requests randomness
+     * @dev Warning: if the VRF response is delayed, avoid calling requestRandomness repeatedly
+     * as that would give miners/VRF operators latitude about which VRF response arrives first.
+     * @dev You must review your implementation details with extreme care.
+     *
+     * @param roller address of the roller
      */
-    function getRandomNumber() public returns (bytes32 requestId) {
+    function rollDice(address roller)
+        public
+        onlyOwner
+        returns (bytes32 requestId)
+    {
         require(
             LINK.balanceOf(address(this)) >= fee,
-            "Not enough LINK - fill contract with faucet"
+            "Not enough LINK to pay fee"
         );
-        return requestRandomness(keyHash, fee);
+        require(s_results[roller] == 0, "Already rolled");
+        requestId = requestRandomness(keyHash, fee);
+        s_rollers[requestId] = roller;
+        s_results[roller] = ROLL_IN_PROGRESS;
+        emit DiceRolled(requestId, roller);
     }
 
     /**
@@ -61,15 +81,26 @@ contract Kuji is VRFConsumerBase, Ownable {
         randomResult = randomness.mod(100).add(1);
     }
 
-        /**
-         * @notice Withdraw LINK from this contract.
-         * @dev this is an example only, and in a real contract withdrawals should
-         * happen according to the established withdrawal pattern: 
-         * https://docs.soliditylang.org/en/v0.4.24/common-patterns.html#withdrawal-from-contracts
-         * @param to the address to withdraw LINK to
-         * @param value the amount of LINK to withdraw
-         */
-        function withdrawLINK(address to, uint256 value) public onlyOwner {
-            require(LINK.transfer(to, value), "Not enough LINK");
-        }
+    /**
+     * @notice Withdraw LINK from this contract.
+     * @dev this is an example only, and in a real contract withdrawals should
+     * happen according to the established withdrawal pattern:
+     * https://docs.soliditylang.org/en/v0.4.24/common-patterns.html#withdrawal-from-contracts
+     * @param to the address to withdraw LINK to
+     * @param value the amount of LINK to withdraw
+     */
+    function withdrawLINK(address to, uint256 value) public onlyOwner {
+        require(LINK.transfer(to, value), "Not enough LINK");
+    }
+
+    function result(address player) public view returns (string memory) {
+        require(s_results[player] != 0, "Dice not rolled");
+        require(s_results[player] != ROLL_IN_PROGRESS, "Roll in progress");
+
+        return getResult(s_results[player]);
+    }
+
+    function getResult(uint256 id) private pure returns (string memory) {
+        return id == WINNING_NUMBER ? "Congrats, you won." : "Sorry, you lost.";
+    }
 }
